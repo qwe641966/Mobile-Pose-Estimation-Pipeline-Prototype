809354d1d254d75a9b3f25b3488ac15901fbd1b2
alex <alexandrosrotsides@gmail.com>
2019-09-17 10:27:28 +0100

    sending results to phone manually

--- get_pose.py
+++ get_pose.py
@@ -11,9 +11,8 @@ run_direct_matching_3D_points_feature_builder = sys.argv[3]
 query_image_arg = sys.argv[4] # i.e IMG_7932.JPG
 data_dir = sys.argv[5] # i.e data/coop3
 intrinsics_matrix_path = sys.argv[6]
-benchmarking  = sys.argv[7] # use this if you are test an images already in the dataset
-
-# run_soft_posit = sys.argv[5]
+extract_3D_points_from_sparse = sys.argv[7]
+benchmarking  = sys.argv[8] # use this if you are testing an images already in the dataset
 
 query_image_arg_no_ext = query_image_arg.split(".")[0]
 
@@ -47,9 +46,12 @@ os.system("python2.7 query_image_feature_extraction.py "+data_dir+" "+query_imag
 print "Running script 3/4"
 print "python2.7 query_matcher_improved_ransac.py "+data_dir+" "+query_image_arg+" "+benchmarking + " " + intrinsics_matrix_path
 os.system("python2.7 query_matcher_improved_ransac.py "+data_dir+" "+query_image_arg+" "+benchmarking + " " + intrinsics_matrix_path)
-# if(run_soft_posit == '1'):
-#     print 'Running softposit script'
-#     run matlab script here
+
 print "Running script 4/4"
 print "python2.7 visualizer.py "+data_dir+"/query_images/"+query_image_arg + " " + benchmarking
 os.system("python2.7 visualizer.py "+data_dir+"/query_images/"+query_image_arg + " " + benchmarking)
+
+if(extract_3D_points_from_sparse == "1"):
+    print "Extracting 3D points from sparse model folder.."
+    print "python2.7 model_points3D_extractor.py "+data_dir
+    os.system("python2.7 model_points3D_extractor.py "+data_dir)
--- view_3D_points_debug_for_pipeline.m
+++ view_3D_points_debug_for_pipeline.m
@@ -1,22 +1,22 @@
-all_3d_points = importdata('data/home_small_boxes/model_points3D.txt');
+all_3d_points = importdata('data/work_alt_desk/model_points3D.txt');
 
-all_3d_points_dm = importdata('results/arcore640/3D_points_direct.txt');
-all_3d_points_image_retrieval = importdata('results/arcore640/3D_points_image_retrieval.txt');
+all_3d_points_dm = importdata('results/arcore/3D_points_direct.txt');
+all_3d_points_image_retrieval = importdata('results/arcore/3D_points_image_retrieval.txt');
 
 % ARCore mobile phone pose
-ar_core_pose = importdata('data/home_small_boxes/ar_core_poses/pose.txt')';
+ar_core_pose = importdata('data/work_alt_desk/ar_core_poses/pose.txt');
 ar_core_pose_R = ar_core_pose(1:3,1:3);
 ar_core_pose_t = ar_core_pose(1:3,4);
 camera_location_arcore = -ar_core_pose_R' * ar_core_pose_t;
 
 % Image retrieval pose:
-pose_img_retrieval = importdata('results/arcore640/RT_image_retrieval.txt');
+pose_img_retrieval = importdata('results/arcore/RT_image_retrieval.txt');
 camera_R_img_retrieval = pose_img_retrieval(1:3, 1:3);
 camera_T_img_retrieval = pose_img_retrieval(1:3, 4);
 camera_location_img_retrieval = -camera_R_img_retrieval' * camera_T_img_retrieval;
 
 % Direct Matching:
-pose_direct_matching = importdata('results/arcore640/RT_direct_matching.txt');
+pose_direct_matching = importdata('results/arcore/RT_direct_matching.txt');
 camera_R_direct_matching = pose_direct_matching(1:3, 1:3);
 camera_T_direct_matching = pose_direct_matching(1:3, 4);
 camera_location_direct_matching = -camera_R_direct_matching' * camera_T_direct_matching;
@@ -29,7 +29,7 @@ homogeneous = ones(size(all_3d_points_image_retrieval(:,1),1),1);
 all_3d_points_image_retrieval = [all_3d_points_image_retrieval homogeneous];
 
 % 3D point in ARCore
-ar_core_3D_points = inv(ar_core_pose) * [[camera_R_img_retrieval camera_T_img_retrieval] ; 0 0 0 1] * all_3d_points_image_retrieval';
+ar_core_3D_points = inv(ar_core_pose) * [[camera_R_img_retrieval camera_T_img_retrieval] ; 0 0 0 1] * all_3d_points_dm';
 ar_core_3D_points = ar_core_3D_points';
 
 figure;
@@ -39,7 +39,14 @@ plot3(0,0,0,'g*');
 hold on
 plotCamera('Location', camera_location_arcore, 'Orientation', ar_core_pose_R, 'Size', 1);
 
-% 
+% write to file
+fid = fopen('ar_core_3D_points_matlab.txt','wt');
+for k = 1:size(ar_core_3D_points,1)
+    fprintf(fid,'%4.5f ',ar_core_3D_points(k,1:3));
+    fprintf(fid,'\n');
+end
+fclose(fid);
+ 
 % figure;
 % plot3(all_3d_points(:,1),all_3d_points(:,2),all_3d_points(:,3),'*');
 % hold on
