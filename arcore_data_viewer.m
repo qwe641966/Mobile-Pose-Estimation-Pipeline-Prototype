1fcf148b4be523f1ef9e172725b7c13eb9438372
alex <alexandrosrotsides@gmail.com>
2019-10-01 18:22:26 +0100

    debug viewer done

--- arcore_data_viewer.m
+++ arcore_data_viewer.m
@@ -1,20 +1,22 @@
 clear all;
 
-points = importdata('matlab_debug_data/data_ar/points.txt');
+points = importdata('matlab_debug_data/data_ar/points3Dworld.txt');
 points = reshape(points, 4, [])'; % (x, y, z, c)
 points = [points(:,1:3) ones(size(points,1),1)];
+correspondences = importdata('matlab_debug_data/data_ar/correspondences.txt');
+
 posemtx = importdata('matlab_debug_data/data_ar/posemtx.txt');
 projmtx = importdata('matlab_debug_data/data_ar/projmtx.txt');
 viewmtx = importdata('matlab_debug_data/data_ar/viewmtx.txt');
 
 ndc = projmtx * viewmtx * points';
-x_screen = (ndc(1,:) + 1) * (1440 / 2 );
-y_screen = (ndc(2,:) + 1) * (2880 / 2 );
+ndc = ndc';
+ndc = ndc ./ ndc(:,4);
 
-x_screen = (ndc(1,:) + 1) * (1440 / 2 );
-y_screen = (ndc(2,:) + 1) * (2880 / 2 );
+x_screen = 1440 * ((ndc(:,1) + 1) / 2);
+y_screen = 2880 * ((1 - ndc(:,2)) / 2 );
 
-xy = [x_screen' y_screen'];
+xy = [x_screen y_screen];
 
 save('matlab_debug_data/data_ar/xy.txt', 'xy', '-ascii', '-double');
 
--- matlab_viewer.py
+++ matlab_viewer.py
@@ -16,14 +16,10 @@ img = cv2.imread("matlab_debug_data/data_ar/frame.jpg")
 for i in range(rows):
     x = int(points2D_projected_direct_matching[i][0])
     y = int(points2D_projected_direct_matching[i][1])
+    print str(x) + ", " + str(y)
     center = (x, y)
     # center = (np.shape(img)[1]-y,x) # weird matlab to python indexing..
     # print "x: " + str(center[0]) + ", y: " + str(center[1])
-    cv2.circle(img, center, 8, (0, 0, 255), -1)
+    cv2.circle(img, center, 12, (0, 0, 255), -1)
 
 cv2.imwrite("matlab_debug_data/data_ar/res.jpg",img)
-
-
-# if(benchmarking != "1"):
-#     os.system("open results/" + query_image_name + "_result_direct_matching.png")
-#     os.system("open results/" + query_image_name + "_result_image_retrieval.png")
